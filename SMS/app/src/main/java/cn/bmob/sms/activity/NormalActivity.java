2f340db876526e3e31ed1384e7d734b0a17a62e8
chaozhouzhang <13760289294@139.com>
2018-11-01 10:13:44 +0800

    Update

--- SMS/app/src/main/java/cn/bmob/sms/activity/NormalActivity.java
+++ SMS/app/src/main/java/cn/bmob/sms/activity/NormalActivity.java
@@ -52,6 +52,9 @@ public class NormalActivity extends AppCompatActivity {
                 }
                 /**
                  * TODO template 如果是自定义短信模板，此处替换为你在控制台设置的自定义短信模板名称；如果没有对应的自定义短信模板，则使用默认短信模板。
+                 *
+                 * TODO 应用名称以及自定义短信内容，请使用不会被和谐的文字，防止发送短信验证码失败。
+                 *
                  */
                 BmobSMS.requestSMSCode(phone, "DataSDK", new QueryListener<Integer>() {
                     @Override
